3e6fe27fa40f7077fc07ff7cd3f45a953b093b56
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-07-30 09:03:17 +0545

    Add pictures

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,27 @@ module.exports = {
       fontFamily:{
          custom1:["Anton", "sans-serif"],
          custom2:["lobster", "sans-serif"],
+      },
+      textShadow: {
+        'default': '2px 2px 4px rgba(0, 0, 0, 0.1)',
+        'md': '4px 4px 8px rgba(0, 0, 0, 0.2)',
+        'lg': '6px 6px 12px rgba(0, 0, 0, 0.3)',
       }
     },
   },
-  plugins: [],
+  plugins: [ function({ addUtilities }) {
+    addUtilities({
+      '.text-shadow': {
+        textShadow: '2px 2px rgb(0, 0, 0)',
+      },
+      '.text-shadow-md': {
+        textShadow: '4px 4px 8px rgba(0, 0, 0)',
+      },
+      '.text-shadow-lg': {
+        textShadow: '6px 6px 12px rgba(0, 0, 0)',
+      },
+    })
+  }
+]
 }
 
